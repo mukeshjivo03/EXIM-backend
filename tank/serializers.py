78233723ea546f6215a3873e76365a7234f0e847
Harshit Singh <harshit62512@gmail.com>
2026-02-25 17:06:55 +0530

    Added Validations

--- tank/serializers.py
+++ tank/serializers.py
@@ -8,11 +8,12 @@ class TankItemSerializer(serializers.ModelSerializer):
         read_only_fields = ['created_at']
         
         
-
 class TankItemColorSerialier(serializers.ModelSerializer):
     class Meta:
         model = TankItem
         fields = ['color' , 'tank_item_name']
+
+
 
 
 class TankDataSerializer(serializers.ModelSerializer):
@@ -21,6 +22,17 @@ class TankDataSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['created_at']
         
+        
+    def validate(self, data):
+        tank_capacity = data.get('tank_capacity')
+        current_capacity = data.get('current_capacity')
+        
+        if tank_capacity is not None and current_capacity is not None:
+            if current_capacity > tank_capacity or current_capacity < 0 or tank_capacity < 0:
+                raise serializers.ValidationError("Current or Tank capacity should be less than zero or more than tank capacity.")
+            
+        return data
+
 class TankDataCapacitySerializer(serializers.ModelSerializer):
     
     item_code = serializers.SlugRelatedField(
@@ -32,3 +44,13 @@ class TankDataCapacitySerializer(serializers.ModelSerializer):
     class Meta:
         model = TankData
         fields = ['current_capacity' , 'item_code']
+        read_only_fields = ['created_at' , 'updated_at' ,'tank_number' ,'is_active' , 'tank_capacity']
+        
+    def validate(self, data):
+        
+        current_capacity = data.get('current_capacity')
+        if self.instance and current_capacity is not None:
+            if current_capacity > self.instance.tank_capacity or current_capacity < 0:
+                raise serializers.ValidationError("Current capacity cannot be more than tanks capacity or less than zero.")
+            
+        return data
