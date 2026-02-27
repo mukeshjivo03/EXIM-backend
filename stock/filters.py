9233c94acc2f6b648deda4a6cb58f48d6673d6c6
Neetu Nair <developer@jivo.in>
2026-02-27 19:01:15 +0530

    Solved Insight API issue

--- stock/filters.py
+++ stock/filters.py
@@ -1,10 +1,10 @@
+from .models import StockStatus
+from django_filters import rest_framework as filters
+
+
 class StockStatusFilters(filters.FilterSet):
     status = filters.ChoiceFilter(choices=StockStatus.STATUS_CHOICES)
-    
-    # Matches card_code in the Party model
     vendor = filters.CharFilter(field_name='vendor_code__card_code')
-    
-    # Matches item_code in the RMProducts model
     item = filters.CharFilter(field_name='item_code__item_code')
 
     class Meta:
