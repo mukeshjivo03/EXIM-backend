0e100d9d8ce975953b09e97c6435d73d84ac6b39
Neetu Nair <developer@jivo.in>
2026-03-02 17:09:17 +0530

    Trend API added

--- daily_price/urls.py
+++ daily_price/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import PriceFetchView
+from .views import PriceFetchView , DailyPriceTrend
 
 urlpatterns = [
-    path('fetch/' , PriceFetchView.as_view())
+    path('fetch/' , PriceFetchView.as_view()),
+    path('trends/', DailyPriceTrend.as_view())
 ]
--- daily_price/views.py
+++ daily_price/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import date, timedelta
+from collections import defaultdict
+from django.http import JsonResponse
 
 from .services import fetch_table_manually
 from .models import DailyPrice
@@ -39,3 +42,37 @@ class PriceFetchView(APIView):
             )
 
             return Response({"status": "Successfully posted to database"})
+
+
+class DailyPriceTrend(APIView):
+    def get(self,request):
+        end_date = date.today()
+        start_date = end_date - timedelta(days=7)
+
+        prices = DailyPrice.objects.filter(
+            date__range = [start_date , end_date]
+        ).order_by('date')
+
+        chart_data = defaultdict(list)
+        unique_dates = []
+
+
+        for p in prices:
+            date_str = p.date.strftime('%b %d')
+            if date_str not in unique_dates:
+                unique_dates.append(date_str)
+
+            chart_data[p.commodity_name].append(float(p.with_gst_ltr))
+
+        datasets = []
+        for commodity, values in chart_data.items():
+            datasets.append({
+                "label": commodity,
+                "data": values,
+                "fill": False
+            })
+
+        return JsonResponse({
+            "labels": unique_dates,
+            "datasets": datasets
+        })
