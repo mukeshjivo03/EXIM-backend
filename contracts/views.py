d109fbd675bdd0dab1f2de7fae3000a8b81d1f90
Harshit Singh <harshit62512@gmail.com>
2026-03-20 13:32:16 +0530

    added permissiona classes for old views

--- contracts/views.py
+++ contracts/views.py
@@ -3,9 +3,12 @@ from rest_framework import generics
 
 from .serializers import DomesticReportSerializer
 from .models import DomesticReports
+from accounts.permissions import IsAdminUser , IsManagerUser
 
 
 class DomesticReportListView(generics.ListAPIView):
+    permission_classes = [IsAdminUser | IsManagerUser]
+    
     queryset = DomesticReports.objects.all()
     serializer_class = DomesticReportSerializer
     
